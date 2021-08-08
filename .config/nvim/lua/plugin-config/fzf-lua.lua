7092f5f2b95650bd655bcd153290f0c86330ce04
Daniel Lindegren <onosendi@dlindegren.com>
2021-08-07 17:30:00 -0700

    More fzf key bindings

--- .config/nvim/lua/plugin-config/fzf-lua.lua
+++ .config/nvim/lua/plugin-config/fzf-lua.lua
@@ -1,5 +1,5 @@
 require('fzf-lua').setup {
-  fzf_layout          = 'reverse',
+  fzf_layout = 'default',
   previewers = {
     bat = {
       theme = 'gruvbox-dark',
@@ -13,6 +13,8 @@ require('fzf-lua').setup {
 local map = vim.api.nvim_set_keymap
 local options = { noremap = true }
 
-map('n', '<Leader>ff', ':FzfLua files<CR>', options)
-map('n', '<Leader>fg', ':FzfLua git_files<CR>', options)
-map('n', '<Leader>fr', ':FzfLua grep<CR>', options)
+map('n', '<Leader>ff', '<cmd>lua require("fzf-lua").files()<CR>', options)
+map('n', '<Leader>fb', '<cmd>lua require("fzf-lua").buffers()<CR>', options)
+map('n', '<Leader>fv', '<cmd>lua require("fzf-lua").git_files()<CR>', options)
+map('n', '<Leader>fg', '<cmd>lua require("fzf-lua").live_grep()<CR>', options)
+map('n', '<Leader>fl', '<cmd>lua require("fzf-lua").grep_last()<CR>', options)
