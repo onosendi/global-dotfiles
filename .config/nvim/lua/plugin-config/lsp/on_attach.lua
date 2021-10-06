2c53f87b139e2864751b34165e3503efedac221b
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-06 13:29:36 -0700

    Remap lsp key bindings

--- .config/nvim/lua/plugin-config/lsp/on_attach.lua
+++ .config/nvim/lua/plugin-config/lsp/on_attach.lua
@@ -1,100 +1,17 @@
+local map = vim.api.nvim_set_keymap
+local options = { noremap = true, silent = true }
+
 return function(client)
-  vim.api.nvim_set_keymap(
-    'n',
-    'gD',
-    '<cmd>lua vim.lsp.buf.declaration()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    'gd',
-    '<cmd>lua vim.lsp.buf.definition()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    'K',
-    '<cmd>lua vim.lsp.buf.hover()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    'gi',
-    '<cmd>lua vim.lsp.buf.implementation()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>k',
-    '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'i',
-    '<C-k>',
-    '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>wa',
-    '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>wr',
-    '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>wl',
-    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>D',
-    '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<space>rn',
-    '<cmd>lua vim.lsp.buf.rename()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    'gr',
-    '<cmd>lua vim.lsp.buf.references()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>d',
-    '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ show_header = false })<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '[d',
-    '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    ']d',
-    '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
-    { noremap = true, silent = true }
-  )
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>q',
-    '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
-    { noremap = true, silent = true }
-  )
+  map('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<CR>', options)
+  map('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', options)
+  map('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', options)
+  map('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>', options)
+  map('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', options)
+  map('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', options)
+  map('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>', options)
+  map('n', '<leader>lw', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ show_header = false })<CR>', options)
+  map('n', '<leader>lp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', options)
+  map('n', '<leader>ln', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', options)
 
   vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
