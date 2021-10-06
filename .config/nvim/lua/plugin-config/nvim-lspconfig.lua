92df6cb7fea7acff4cc687aae8ea0b45a50fb80e
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-06 12:51:51 -0700

    Configure LSP diagnostics signs

--- .config/nvim/lua/plugin-config/nvim-lspconfig.lua
+++ .config/nvim/lua/plugin-config/nvim-lspconfig.lua
@@ -3,6 +3,39 @@
 -- npm i -g pyright
 -- npm i -g bash-language-server
 
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    underline = false,
+    virtual_text = {
+      prefix = '●',
+      spacing = 2,
+    },
+    update_in_insert = true,
+    severity_sort = true,
+  }
+)
+
+vim.fn.sign_define('LspDiagnosticsSignError', {
+  text = '✖',
+  numhl = 'LspDiagnosticsDefaultError',
+})
+
+vim.fn.sign_define('LspDiagnosticsSignWarning', {
+  text = '▲',
+  numhl = 'LspDiagnosticsDefaultWarning',
+})
+
+vim.fn.sign_define('LspDiagnosticsSignInformation', {
+  text = '●',
+  numhl = 'LspDiagnosticsDefaultInformation',
+})
+
+vim.fn.sign_define('LspDiagnosticsSignHint', {
+  text = '✱',
+  numhl = 'LspDiagnosticsDefaultHint',
+})
+
 require 'plugin-config/lsp/bash';
 require 'plugin-config/lsp/css';
 require 'plugin-config/lsp/html';
