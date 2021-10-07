0cd6e5d567320abdb1119a2229cd734b466c64fd
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-07 14:55:47 -0700

    Underline diagnostic errors

--- .config/nvim/lua/plugin-config/nvim-lspconfig.lua
+++ .config/nvim/lua/plugin-config/nvim-lspconfig.lua
@@ -22,7 +22,7 @@ vim.cmd [[
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
-    underline = false,
+    underline = true,
     virtual_text = {
       prefix = '●',
       spacing = 2,
