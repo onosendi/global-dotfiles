c1c31d97a9b6fddf405bac0f1a1c0745145dddff
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-15 14:18:45 -0700

    Remove nvim-lsp-ts-utils defaults

--- .config/nvim/lua/plugins/nvim-lspconfig/tsserver.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig/tsserver.lua
@@ -3,40 +3,10 @@ local null_ls = require'null-ls'
 local u = require('utils')
 
 local ts_utils_settings = {
+  cwd = function(params)
+    return require('lspconfig.util').root_pattern('tsconfig.json')(params.bufname)
+  end,
   debug = false,
-  disable_commands = false,
-  enable_import_on_completion = false,
-
-  -- import all
-  import_all_timeout = 5000, -- ms
-  import_all_priorities = {
-    buffers = 4, -- loaded buffer names
-    buffer_content = 3, -- loaded buffer content
-    local_files = 2, -- git files or files with relative path markers
-    same_file = 1, -- add to existing import statement
-  },
-  import_all_scan_buffers = 100,
-  import_all_select_source = false,
-
-  -- eslint
-  eslint_enable_code_actions = true,
-  eslint_enable_disable_comments = true,
-  eslint_bin = 'eslint_d',
-  eslint_enable_diagnostics = false,
-
-  -- formatting
-  enable_formatting = false,
-  formatter = 'prettier',
-  formatter_opts = {},
-
-  -- update imports on file move
-  update_imports_on_move = false,
-  require_confirmation_on_move = false,
-  watch_dir = nil,
-
-  -- filter diagnostics
-  filter_out_diagnostics_by_severity = {},
-  filter_out_diagnostics_by_code = {},
 }
 
 local M = {}
