170d671d8412cd56e4de00a06e9d3067553990ad
Daniel Lindegren <onosendi@dlindegren.com>
2021-12-29 15:58:21 -0700

    Update null-ls to configure options in `setup`

--- .config/nvim/lua/plugins/nvim-lspconfig/null-ls.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig/null-ls.lua
@@ -2,11 +2,11 @@ local null_ls = require'null-ls'
 
 local M = {}
 M.setup = function(on_attach)
-  null_ls.config {
+  null_ls.setup {
+    on_attach = on_attach,
     diagnostics_format = '#{m} [#{c}]',
     debounce = 150,
   }
-  require'lspconfig'['null-ls'].setup { on_attach = on_attach }
 end
 
 return M
