fda372cb121259fa1735cd6d6bb809f4e8ffb1cd
Daniel Lindegren <onosendi@dlindegren.com>
2022-01-02 15:44:52 -0700

    vim-fugitive hotkeys

---
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -0,0 +1,9 @@
+local map = vim.api.nvim_set_keymap
+local options = { noremap = true }
+
+map('n', '<Leader>gg', ':Git<CR>', options)
+map('n', '<Leader>gs', ':Git status<CR>', options)
+map('n', '<Leader>gcc', ':Git commit<CR>', options)
+map('n', '<Leader>gca', ':Git commit -a<CR>', options)
+map('n', '<Leader>gp', ':Git push<CR>', options)
+map('n', '<Leader>gdv', ':Gvdiffsplit<CR>', options)
