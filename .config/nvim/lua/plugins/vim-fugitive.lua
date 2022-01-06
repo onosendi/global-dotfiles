fddeccd2b635000fb322b352a209071c4773ac16
Daniel Lindegren <onosendi@dlindegren.com>
2022-01-06 11:47:49 -0700

    Add `Git l` alias for fugitive

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -2,6 +2,7 @@ local map = vim.api.nvim_set_keymap
 local options = { noremap = true }
 
 map('n', '<Leader>gg', ':Git<CR>', options)
+map('n', '<Leader>gl', ':Git l<CR>', options)
 map('n', '<Leader>gs', ':Git status<CR>', options)
 map('n', '<Leader>gcc', ':Git commit<CR>', options)
 map('n', '<Leader>gca', ':Git commit -a<CR>', options)
