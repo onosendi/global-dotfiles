e02e3aa8470a9aea1170c2a78ed5560dd49b028b
Daniel Lindegren <onosendi@dlindegren.com>
2023-10-22 13:42:52 -0700

    Fix indent blank line

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,9 +1,1 @@
-vim.cmd [[
-    highlight IndentBlanklineIndent1 guifg=#4C4C4C gui=nocombine
-]]
-
-require("indent_blankline").setup {
-    char_highlight_list = {
-        'IndentBlanklineIndent1',
-    },
-}
+require("ibl").setup {}
