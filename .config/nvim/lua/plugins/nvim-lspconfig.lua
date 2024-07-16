34497a91897f48aaf746f827299ea2d1c5723845
Daniel Lindegren <onosendi@dlindegren.com>
2024-07-15 19:20:22 -0700

    More nvim config

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -117,23 +117,7 @@ return {
     mason_lspconfig.setup_handlers({
       -- default handler for installed servers
       function(server_name)
-        print(server_name)
         lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["html"] = function()
-        lspconfig["html"].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["pyright"] = function()
-        lspconfig["pyright"].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["cssls"] = function()
-        lspconfig["cssls"].setup({
           capabilities = capabilities,
         })
       end,
