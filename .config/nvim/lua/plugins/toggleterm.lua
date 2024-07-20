043d5dc0fd0f0d013a8846e1aab44ca082d70772
Daniel Lindegren <onosendi@dlindegren.com>
2024-07-20 16:54:30 -0700

    nvim: show numbers in toggleterm buffer

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -8,7 +8,10 @@ return {
       size = 20,
       shade_terminals = false,
       shading_factor = 0,
-      open_mapping = [[<c-\>]]
+      open_mapping = [[<c-\>]],
+      on_open = function()
+        vim.cmd("setlocal number")
+      end,
     })
 
     -- Start in insert mode whenever switching to a terminal buffer.
