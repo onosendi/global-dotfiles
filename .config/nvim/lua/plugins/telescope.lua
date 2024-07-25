06456465cfff362169ada2d5c19932792ec1e878
Daniel Lindegren <onosendi@dlindegren.com>
2024-07-25 09:14:48 -0700

    nvim: add telescope grep_string

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -26,6 +26,7 @@ return {
     { "<leader>ff", ":Telescope find_files<CR>", mode = { "n" }, desc = "Lists files in your current working directory, respects .gitignore" },
     { "<leader>fg", ":Telescope live_grep<CR>", mode = { "n" }, desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore. (Requires ripgrep)" },
     { "<leader>fh", ":Telescope help_tags<CR>", mode = { "n" }, desc = "" },
+    { "<leader>fw", ":Telescope grep_string<CR>", mode = { "n" }, desc = "Search for the word under your cursor in your current working directory" },
     { "<leader>fk", ":Telescope keymaps<CR>", mode = { "n" }, desc = "" },
   },
 }
